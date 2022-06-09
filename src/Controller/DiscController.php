d7a8e8d9bd1b6c1a94926c1074d96006e446756f
Yoel Velasquez Valencia <yoel.velasquez.valencia@gmail.com>
2022-06-09 18:33:44 -0500

    Alter add disc items data and custom parent templating

--- src/Controller/DiscController.php
+++ src/Controller/DiscController.php
@@ -6,23 +6,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController extends AbstractController
+class DiscController extends AbstractController
 {
     #[Route('/')]
     public function index(): Response
     {
 
         $data = [
-            ['name' => 'Name music', 'desc' => 'Desc music'],
-            ['name' => 'Name music', 'desc' => 'Desc music'],
-            ['name' => 'Name music', 'desc' => 'Desc music'],
-            ['name' => 'Name music', 'desc' => 'Desc music'],
-            ['name' => 'Name music', 'desc' => 'Desc music']
+            ['name' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['name' => 'Waterfalls', 'artist' => 'TLC'],
+            ['name' => 'Creep', 'artist' => 'Radiohead'],
+            ['name' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['name' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['name' => 'Fantasy', 'artist' => 'Mariah Carey'],
         ];
-
         return $this->render('disc/index.html.twig', [
-            'data' => $data
-        ]); 
+            'title' => 'Artist name',
+            'data' => $data,
+        ]);
 
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
+    {
+        $title = (($slug))? 'Genre: '.ucfirst(str_replace(['-', '_'], ' ', $slug)):'All Genres';
+        return new Response($title);
     }
 }

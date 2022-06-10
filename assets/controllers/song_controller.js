d0dc90b656849e5f5bc975337395a726ca5607fb
Yoel Velasquez Valencia <yoel.velasquez.valencia@gmail.com>
2022-06-10 08:22:55 -0500

    Upgrade js packages add axios and refactorin js playing controller

---
+++ assets/controllers/song_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from '@hotwired/stimulus';
+
+import axios from 'axios';
+
+export default class extends Controller {
+
+    static values = {
+        infoUrl: String
+    };
+
+    play(event){
+
+        event.preventDefault();
+
+        axios.get(this.infoUrlValue)
+            .then((response) => {
+                
+                //console.log(response);
+
+                const audio = new Audio(response.data.url);
+                audio.play();
+
+            });
+
+    }
+}
